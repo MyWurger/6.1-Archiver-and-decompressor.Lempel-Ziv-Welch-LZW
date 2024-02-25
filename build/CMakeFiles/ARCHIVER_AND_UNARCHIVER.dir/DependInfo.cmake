edffc423d592802d7004161dfa77a425907d625f
MyWurger <wdgwdg9@gmail.com>
2024-02-25 17:11:45 +0300

    Добавлена функциональность кодирования и декодирования

--- build/CMakeFiles/ARCHIVER_AND_UNARCHIVER.dir/DependInfo.cmake
+++ build/CMakeFiles/ARCHIVER_AND_UNARCHIVER.dir/DependInfo.cmake
@@ -1,15 +1,15 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
+  "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/dmitru/Проекты VisualCode/Archiver_and_Unarchiver/main.c" "/home/dmitru/Проекты VisualCode/Archiver_and_Unarchiver/build/CMakeFiles/ARCHIVER_AND_UNARCHIVER.dir/main.c.o"
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dmitru/Проекты VisualCode/Archiver_and_Unarchiver/main.cpp" "/home/dmitru/Проекты VisualCode/Archiver_and_Unarchiver/build/CMakeFiles/ARCHIVER_AND_UNARCHIVER.dir/main.cpp.o"
   )
-set(CMAKE_C_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # Targets to which this target links.
